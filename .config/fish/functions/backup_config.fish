2fde543e8dd7577652ea55f5b32bf09dee30eb47
lxl66566 <18259734087@163.com>
2023-05-23 00:01:56 +0800

    20230523-00:01:56

--- .config/fish/functions/backup_config.fish
+++ .config/fish/functions/backup_config.fish
@@ -1,6 +1,7 @@
 function backup_config --description 'backup my config on my ArchWSL'
 cd ~/myconfig
 cp -rfu ~/.config .
+cp -fu ~/.tmux.conf .
 git add .
 git commit -m (date "+%Y%m%d-%H:%M:%S")
 git status
